40c3e70cfc29f1c34fd6086436bdd71f3ff1afb3
vblazhes <62813356+vblazhes@users.noreply.github.com>
2020-06-23 17:07:16 +0200

    project initialized

---
+++ snack-machine-management/src/main/java/mk/ukim/finki/emt/dormitorysmartsnackmachine/snackmachinemanagement/SnackMachineManagementApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.dormitorysmartsnackmachine.snackmachinemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackMachineManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnackMachineManagementApplication.class, args);
+    }
+
+}
