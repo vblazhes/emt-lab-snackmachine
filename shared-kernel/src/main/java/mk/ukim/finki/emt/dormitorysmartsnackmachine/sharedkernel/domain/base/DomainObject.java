695d5786a370371d6463ccd697d800b7c1af4964
vblazhes <62813356+vblazhes@users.noreply.github.com>
2020-06-24 14:46:36 +0200

    shared-kernel domain objects hierarchy and global Value Objects

--- shared-kernel/src/main/java/mk/ukim/finki/emt/dormitorysmartsnackmachine/sharedkernel/domain/base/DomainObject.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.emt.dormitorysmartsnackmachine.sharedkernel.domain.base;
-
-import java.io.Serializable;
-
-/**
- * Marker interface for domain objects.
- */
-public interface DomainObject extends Serializable {
-}
