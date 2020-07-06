9bd7c980e40a964ba82116142771672438d9af8d
FisnikL <fisnik.limani1@gmail.com>
2020-07-06 12:00:11 +0200

    remove project

--- shared-kernel/src/main/java/mk/ukim/finki/emt/dormitorysmartsnackmachine/sharedkernel/domain/base/ValueObject.java
+++
@@ -1,7 +0,0 @@
-package mk.ukim.finki.emt.dormitorysmartsnackmachine.sharedkernel.domain.base;
-
-/**
- * Marker interface for value objects.
- */
-public interface ValueObject extends DomainObject {
-}
--- snack-machine-management/src/main/java/mk/ukim/finki/emt/dormitorysmartsnackmachine/snackmachinemanagement/domain/repository/SnackMachineRepository.java
+++
@@ -1,8 +0,0 @@
-package mk.ukim.finki.emt.dormitorysmartsnackmachine.snackmachinemanagement.domain.repository;
-
-import mk.ukim.finki.emt.dormitorysmartsnackmachine.snackmachinemanagement.domain.model.SnackMachine;
-import mk.ukim.finki.emt.dormitorysmartsnackmachine.snackmachinemanagement.domain.model.identifier.SnackMachineId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SnackMachineRepository extends JpaRepository<SnackMachine, SnackMachineId> {
-}
